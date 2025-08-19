294d4c5a2f8a298df7df6fa625d3fbff428296b6
Myles-Kipngeno <kipngenomyles@gmail.com>
2025-08-19 16:23:07 +0300

    Aitc exams

---
+++ myles.py
@@ -0,0 +1,9 @@
+# 1. a)Define a variable? A variable is one of python's key properties used to asign a value.
+#    b)List 4  simple data types used in python? int,bool,list,dictionary,tuple,set
+# 
+# 2. Write a python program that adds two numbers provided by the user?
+numbers = eval(input("Enter a nummber: "))
+number = eval(input("Enter a nummber: "))
+sum = numbers + number
+print(sum)
+
